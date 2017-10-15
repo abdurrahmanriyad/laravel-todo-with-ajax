bf79ba565c05e2e86a701ca8d2b53c0d38e0333e
Abdur Rahman <primez@Abdurs-MacBook-Pro.local>
2017-10-15 23:33:30 +0600

    added view for adding item

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,11 +5,56 @@
     <title>Ajax todo list project</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
 
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-offset-3 col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Laravel Todo with ajax
+                            <a href="#" class="pull-right" data-toggle="modal" data-target="#editList"><i class="fa fa-plus" aria-hidden="true"></i></a>
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">Cras justo odio</li>
+                            <li class="list-group-item">Dapibus ac facilisis in</li>
+                            <li class="list-group-item">Morbi leo risus</li>
+                            <li class="list-group-item">Porta ac consectetur ac</li>
+                            <li class="list-group-item">Vestibulum at eros</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
+            {{--==== modal =====--}}
+            <div class="modal fade" tabindex="-1" role="dialog" id="editList">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">Add New Item</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p><input type="text" class="form-control" id="addListItem"></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal" style="display: none;">Close</button>
+                            <button type="button" class="btn btn-primary" style="display: none;">Save changes</button>
+                            <button type="button" class="btn btn-primary" id="addListBtn">Add Item</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
 
+        </div>
+    </div>
 
 <script
         src="http://code.jquery.com/jquery-3.2.1.min.js"
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('list', function () {
+    return view('list');
+});
+
